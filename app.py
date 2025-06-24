9a5c385d09005333efb2214b3bd81b0776315c7c
Jonathan Abebe <jonathanabebe@eduroam-172-031-130-109.pool.rrze.net>
2025-06-24 22:23:56 +0200

    Modularisierung der Plotfunktionen und Aufräumen der Struktur (utils/plot_utils, Layout-Importe angepasst)

--- app.py
+++ app.py
@@ -213,6 +213,6 @@ def highlight_active_link(pathname):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 8051))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
     
     
